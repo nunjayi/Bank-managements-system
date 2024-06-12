e8fa76dabfd45d4be3283763648669b494a1e289
nunjayi <epenyash@gmail.com>
2024-06-12 09:42:43 +0300

    adding transaction and bank report asset balance in bank

---
+++ lib/bank.py
@@ -0,0 +1,10 @@
+class Bank:
+    def __init__(self,name):
+        self.name = name
+        self.branches = []
+        self.accounts = []
+        self.managers = []
+        self.asset_balance = 0
+    def report_asset_balance(self):
+        pass
+        
---
+++ lib/transaction.py
@@ -0,0 +1,12 @@
+class Transaction:
+    def __init__(self, transaction_id,Account_no,User_id,transaction_type,amount):
+        self.transaction_id = transaction_id
+        self.Account_no = Account_no
+        self.User_id = User_id
+        self.transaction_type = transaction_type
+        self.amount = amount
+        self.time = #date is here
+
+    def add_transaction(self):
+        # add to database
+        pass
