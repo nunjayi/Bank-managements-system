702e48d30457c7a8645941c0b1d44cf5b40e27fb
pr-i-nce <162322860+pr-i-nce@users.noreply.github.com>
2024-06-13 12:55:37 +0300

    Update account.py

--- lib/Models/account.py
+++ lib/Models/account.py
@@ -5,10 +5,10 @@ class Account:
 
     def __init__(self, account_id=None, user_id=None, branch_id=None, account_type=None, account_balance=0):
         self._account_id = Account.account_counter if account_id is None else account_id
-        self.user_id = user_id   # INT FK
-        self.branch_id = branch_id # INT FK
-        self.account_type = account_type # TEXT
-        self._account_balance = account_balance # FLOAT
+        self.user_id = user_id   # Assuming INT foreign key
+        self.branch_id = branch_id  # Assuming INT foreign key
+        self.account_type = account_type  # Assuming TEXT
+        self._account_balance = account_balance  # Assuming FLOAT
         if account_id is None:
             Account.account_counter += 1
     
@@ -25,14 +25,14 @@ class Account:
             self._account_balance += amount
             self.log_transaction('Deposit', amount)
         else:
-            print(f"Invalid deposit amount: KSh{amount}")
+            print(f"Invalid deposit amount: {amount}")
 
     def withdraw_account(self, amount):
         if amount <= self._account_balance:
             self._account_balance -= amount
             self.log_transaction('Withdrawal', amount)
         else:
-            print(f"Invalid withdrawal amount: KSh{amount}")
+            print(f"Invalid withdrawal amount: {amount}")
 
     def transfer_account(self, target_account, amount):
         if amount <= self._account_balance:
@@ -40,8 +40,9 @@ class Account:
             target_account._account_balance += amount
             self.log_transaction('Transfer Out', amount, target_account.account_id, target_account.user_id)
             target_account.log_transaction('Transfer In', amount, self.account_id, self.user_id)
+            print(f"Transfer of {amount} from account ID {self.account_id} to account ID {target_account.account_id} successful.")
         else:
-            print(f"Invalid transfer amount: KSh{amount}")
+            print(f"Insufficient balance to transfer {amount} from account ID {self.account_id}.")
 
     def log_transaction(self, transaction_type, amount, target_account_id=None, target_user_id=None):
         transaction = Transaction(
@@ -54,3 +55,12 @@ class Account:
             target_user_id=target_user_id
         )
         transaction.add_transaction()
+
+    def display_account_info(self):
+        print(f"Account ID: {self.account_id}")
+        print(f"User ID: {self.user_id}")
+        print(f"Branch ID: {self.branch_id}")
+        print(f"Account Type: {self.account_type}")
+        print(f"Account Balance: {self.account_balance}")
+
+
